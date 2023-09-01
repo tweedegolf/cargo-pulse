6cdf600db5af5e186984048153755ff293ebf5fd
Marc Schoolderman <marc@tweedegolf.com>
2023-09-01 11:50:38 +0200

    use tokio::main

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,19 @@ async fn display_vital_signs<'a>(crates: impl IntoIterator<Item = &'a str>) {
     }
 }
 
-fn main() {
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async {
-            //TODO: use gumdrop for a fuller command line parser, see cargo-minify
-            if let Some(name) = std::env::args().nth(1) {
-                display_vital_signs([name.as_str()]).await;
-            } else {
-                let metadata = MetadataCommand::new().no_deps().exec().unwrap();
-                display_vital_signs(
-                    metadata.packages[0]
-                        .dependencies
-                        .iter()
-                        .map(|dep| dep.name.as_str()),
-                )
-                .await;
-            };
-        })
+#[tokio::main]
+async fn main() {
+    //TODO: use gumdrop for a fuller command line parser, see cargo-minify
+    if let Some(name) = std::env::args().nth(1) {
+        display_vital_signs([name.as_str()]).await;
+    } else {
+        let metadata = MetadataCommand::new().no_deps().exec().unwrap();
+        display_vital_signs(
+            metadata.packages[0]
+                .dependencies
+                .iter()
+                .map(|dep| dep.name.as_str()),
+        )
+        .await;
+    };
 }
